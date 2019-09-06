88b03b67d1d1bf5641ce58e14c36bfe65ad6169f
Cristian Gabor <cristi26.gabor@gmail.com>
2019-09-07 02:53:10 +0300

    Added jquery functionality

---
+++ Licenta2019Project/static/js/home_live.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#flip").click(function() {
+        $("#panel").slideDown("slow");
+    });
+});
+
+$(document).ready(function() {
+    $("#panel").click(function() {
+        $("#panel").slideUp("slow");
+    });
+});
